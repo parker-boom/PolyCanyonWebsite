49f0e16e9e226d5809f73a4a894c33aec550f0a6
Parker Jones <pjones15@calpoly.edu>
2024-09-14 14:20:30 -0700

    Support page implemented

--- poly-canyon-website/src/index.js
+++ poly-canyon-website/src/index.js
@@ -2,17 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import DownloadPage from './downloads/DownloadPage';
-
-
+import SupportPage from './support/SupportPage';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <DownloadPage />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/support" element={<SupportPage />} />
+        <Route path="/" element={<DownloadPage />} />
+      </Routes>
+    </Router>
   );
 }
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
